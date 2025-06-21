c26cbd3374577c6b3c4fcfae9de4645184754c57
VERA-OBIERO <vera.obiero@gmail.com>
2025-06-21 16:14:05 +0300

    packages

--- main.go
+++ main.go
@@ -63,14 +63,6 @@ func getFirstNames() []string {
 
 }
 
-func validateUserInput(firstName string, lastName string, emailAddress string, userTickets uint) (bool, bool, bool) {
-	isValidName := len(firstName) >= 2 && len(lastName) >= 2
-	isValidEmail := strings.Contains(emailAddress, "@")
-	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
-	return isValidName, isValidEmail, isValidTicketNumber
-
-}
-
 func getUserInput() (string, string, string, uint) {
 
 	var firstName string
